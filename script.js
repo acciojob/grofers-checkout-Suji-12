b435557386037b77ebfc5cd5fa18973b6bcf0fd0
Suji-12 <23csec23sujithas@gmail.com>
2025-11-18 15:12:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const priceCells = document.querySelectorAll(".prices");
+	let total = 0;
+
+	priceCells.forEach(cell => {
+		total += Number(cell.textcontent.trim());
+	});
+
+	const table = document.querySelector(|"table");
+
+
+	const totalRow = document.createElement("tr");
+	const totalcell = document.createElement("td");
+
+	totalCell.colSpan = 2;
+
+	totalcell.textcontent = "Total Price: " + total;
+
+	totalRow.appendChild(totalcell);
+	table.appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
